726b907d1022bd979e6e1bbe1a9e178df554a299
mail2ravi003 <mail2ravi003@gmail.com>
2023-05-14 13:33:59 +0530

    Update

---
+++ dist/custom.js
@@ -0,0 +1,15 @@
+function openvideo() {
+  document.getElementById("video").style.display = "block";
+}
+
+function closevideo() {
+  document.getElementById("video").style.display = "none";
+}
+
+function openchat() {
+  document.getElementById("chat").style.display = "block";
+}
+
+function closechat() {
+  document.getElementById("chat").style.display = "none";
+}
