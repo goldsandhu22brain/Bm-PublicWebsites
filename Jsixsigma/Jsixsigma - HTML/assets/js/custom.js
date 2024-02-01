0cd78f91039ed1550811de06b615391df28d38a8
Satish <satishlathiya1999@gmail.com>
2024-02-01 12:13:54 +0530

    Failure mode and MSE master

--- Jsixsigma/Jsixsigma - HTML/assets/js/custom.js
+++ Jsixsigma/Jsixsigma - HTML/assets/js/custom.js
@@ -1,75 +1,74 @@
-/*--------------------------------------*/
+/* ========================================== 
+MENU RESPONSIVE
+========================================== */
+$(function () {
+    const header = $('header').outerHeight();
+    const burgerMenu = document.getElementById("burger");
+    const navbarMenu = document.getElementById("menu-nav");
 
-/* Fix Header
-/*--------------------------------------*/
-	$(window).scroll(function() {     
-		var scroll = $(window).scrollTop();
-		if (scroll > 10) {
-			$("#header").addClass("active");
-		}
-		else {
-			$("#header").removeClass("active");
-		}
-	});
-/*--------------------------------------*/
+    // Show and Hide Navbar Menu
+    burgerMenu.addEventListener("click", () => {
+        burgerMenu.classList.toggle("is-active");
+        navbarMenu.classList.toggle("is-active");
 
-/* Back to Top
-/*--------------------------------------*/
+        if (navbarMenu.classList.contains("is-active")) {
+            //   navbarMenu.style.height = 400+ "px";
+            $("#menu").css("height", "calc(100vh - " + header + "px)")
+            //  navbarMenu.style.maxHeight = navbarMenu.scrollHeight + "px";
+
+        } else {
+            navbarMenu.removeAttribute("style");
+        }
+    });
+});
+
+// below function to add purple bg in header whenever user scroll down for better visibility - 02-01-24 Tosif
+
 $(window).scroll(function () {
-	if ($(this).scrollTop() > 650) {
-		$('#scrollTop').fadeIn();
-	} else {
-		$('#scrollTop').fadeOut();
+    var nav = $('.d-nav');
+    var top = 50;
+    if ($(window).scrollTop() >= top) {
+
+        nav.addClass('nav-bg-purple');
+
+    } else {
+        nav.removeClass('nav-bg-purple');
+    }
+});
+
+// text transcript below 02-02-24 Tosif
+function ValidateTranscript() {
+
+    var message = '';
+    var isValid = true;
+    if ($('#txtTranscriptNoNew').val().trim() == '') {
+
+        isValid = false;
+        message += 'Please enter transcript no.';
+
+    }
+    if ($('#txtTranscriptNoNew').val().trim() != '') {
+        if (!parseInt($('#txtTranscriptNoNew').val())) {
+            isValid = false;
+            message += 'Please enter transcript no. in numeric format.';
+
+        }
+        else if ($('#txtTranscriptNoNew').val().length > 20) {
+            isValid = false;
+            message += 'Length of transcript no. cannot exceed 20 characters.';
+            // window.location.href = "../Individual/Transcript.aspx?transcriptid=" + $('#txtTranscriptNoNew').val();
+        }
+    }
+
+    if (!isValid) {
+        //message += '</ul>';
+        alert(message);
+		    return false;
+
+    }
+	else
+	{
+		window.location.href = "Transcript.aspx?transcriptid=" + $('#txtTranscriptNoNew').val();
 	}
-});
-// scroll up function
-$('#scrollTop').click(function () {
-	$('html, body').animate({ scrollTop: 0 }, 650);
-});
-/*--------------------------------------*/
+}
 
-/* Effect
-/*--------------------------------------*/
-AOS.init({
-	disable: 'mobile',
-	duration: 1000
-});
-/*--------------------------------------*/
-
-$(document).ready(function () {
-	$(".specialities-slider").slick({
-		centerMode: true,
-		centerPadding: '0',
-		slidesToShow: 4,
-		slidesToScroll: 1,
-		autoplay: true,
-		speed: 1200,
-		autoplaySpeed: 1200,	
-		dots: false,
-		pauseOnHover: true,
-		arrows: true,
-		infinite: true,
-		
-		responsive: [
-		{
-		  breakpoint: 991,
-		  settings: {
-			slidesToShow: 3,
-		  }
-		},
-		{
-		  breakpoint: 767,
-		  settings: {
-			slidesToShow: 2,
-		  }
-		},
-		{
-		  breakpoint: 575,
-		  settings: {
-			slidesToShow: 1,
-		  }
-		},
-	  ]
-		
-	});
-});
