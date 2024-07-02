36f8e6f4e5ce65874259acb3d7abab31a133f3ff
mail2ravi003 <131131959+mail2ravi003@users.noreply.github.com>
2024-07-02 22:43:44 +0530

    Update interviewNewDesign-custom.js

--- portal/assets/js/interviewNewDesign-custom.js
+++ portal/assets/js/interviewNewDesign-custom.js
@@ -1,1 +1,15 @@
+/*--------------------------------------*/
 
+/*Date Range
+/*--------------------------------------*/
+$(function() {
+  $('input[name="datetimes"]').daterangepicker({
+    timePicker: true,
+    startDate: moment().startOf('hour'),
+    endDate: moment().startOf('hour').add(32, 'hour'),
+    locale: {
+      format: 'M/DD/YYYY hh:mm A'
+    }
+  });
+});
+/*--------------------------------------*/
